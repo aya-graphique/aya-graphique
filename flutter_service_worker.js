a706731907fde75d372050bcd473711feac05950
aya-graphique <aya-graphique@users.noreply.github.com>
2026-08-05 21:29:03 +0000

    deploy: adbe99788712126bfc45ef28b5a63227f88bde10

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,14 +4,14 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "b8f4757a403193778e9e17c996107024",
-"flutter_bootstrap.js": "e9a05613e60d1fe9fef71a03b7c1adb7",
+"flutter_bootstrap.js": "c638bb2e05df46a35686595c04e72628",
 "icons/Icon-192.png": "09da65d56a7ebd856dad194c0808d74f",
 "icons/Icon-512.png": "08c6b0f0cea086fea921cf52bc27d4da",
 "icons/Icon-maskable-512.png": "08c6b0f0cea086fea921cf52bc27d4da",
 "icons/Icon-maskable-192.png": "09da65d56a7ebd856dad194c0808d74f",
 "index.html": "f1a214ab1e58c8cc14c3ecbbb27a316a",
 "/": "f1a214ab1e58c8cc14c3ecbbb27a316a",
-"main.dart.js": "050a3781c4e3f4b37e91811d7388f27f",
+"main.dart.js": "5ac80a0dd23e8f0f5bca2f361e9450e8",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "googlefd0edbfdde9e9f83.html": "d393302aa072d3e16449ba62e8dc7b60",
 "canvaskit/skwasm.wasm": "f0dfd99007f989368db17c9abeed5a49",
