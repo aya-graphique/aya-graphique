9d9784d86fa8cc5705be84d54df38d657c0a0f45
aya-graphique <aya-graphique@users.noreply.github.com>
2026-08-31 08:06:51 +0000

    deploy: 50aa1bf1e83f7729d9d3a137f62c674fcea0d1ad

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3766824559"
+    serviceWorkerVersion: "2243297836"
   }
 });
