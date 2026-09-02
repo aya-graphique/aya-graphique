8e5a8bfbe886d40d05939faae22f0617d10ad191
aya-graphique <aya-graphique@users.noreply.github.com>
2026-09-02 02:54:03 +0000

    deploy: 036462a0c38ee75f2eedc44d0024ca57a069d060

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1370518650"
+    serviceWorkerVersion: "2473477371"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -66,9 +66,9 @@ const RESOURCES = {"version.json": "616c99fb8e49e8ff4b24e2d6efccb5a5",
 "assets/assets/icon/app_icon.png": "22a17caa4bff938e45aff60e915361ab",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
-"main.dart.js": "623bf5672f576e0e9f1d88d5b168a076",
+"main.dart.js": "3653480bb5d095d7d0c909655b46e9c0",
 "googlefd0edbfdde9e9f83.html": "d393302aa072d3e16449ba62e8dc7b60",
-"flutter_bootstrap.js": "96b08d0e998bda5d5c0451772d8bde29",
+"flutter_bootstrap.js": "a0067d3b9b9bdb9363d6b09e4a0f51d2",
 "manifest.json": "d2e7d15a9aea41cdbf334dd74edfe7e7"};
 // The application shell files that are downloaded before a service worker can
 // start.
