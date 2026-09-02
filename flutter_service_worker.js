e3be912d69c0f3585d818c5d719a504d051fb75a
aya-graphique <aya-graphique@users.noreply.github.com>
2026-09-02 02:41:15 +0000

    deploy: 608f4669291d1b10dbf366b93434f7ebf7485b75

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -66,9 +66,9 @@ const RESOURCES = {"version.json": "616c99fb8e49e8ff4b24e2d6efccb5a5",
 "assets/assets/icon/app_icon.png": "22a17caa4bff938e45aff60e915361ab",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
-"main.dart.js": "b5127a1edf4a4b76749f9b61f9769862",
+"main.dart.js": "623bf5672f576e0e9f1d88d5b168a076",
 "googlefd0edbfdde9e9f83.html": "d393302aa072d3e16449ba62e8dc7b60",
-"flutter_bootstrap.js": "ad5aa4e3f9eef8b48c3e2d396013e0f7",
+"flutter_bootstrap.js": "96b08d0e998bda5d5c0451772d8bde29",
 "manifest.json": "d2e7d15a9aea41cdbf334dd74edfe7e7"};
 // The application shell files that are downloaded before a service worker can
 // start.
